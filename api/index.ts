e143620d4942b3a30ac6470c1c282776a1b3cb24
SrEloyF <fabrizioeloys@gmail.com>
2026-03-06 16:33:05 -0500

    vercel settings v12

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,27 @@
 require('ejs');
+
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from '../src/app.module';
 import { join } from 'path';
+import fs from 'fs';
 
 export default async function handler(req: any, res: any) {
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  console.log('ROOT FILES:', fs.readdirSync(process.cwd()));
+  console.log('SRC FILES:', fs.readdirSync(join(process.cwd(), 'src')));
 
-  const viewsPath = join(process.cwd(), 'src/views');
+  const possiblePaths = [
+    join(process.cwd(), 'views'),
+    join(process.cwd(), 'src/views'),
+    join(__dirname, '../views'),
+    join(__dirname, '../src/views'),
+  ];
 
-  console.log('ViewsPath:', viewsPath);
+  let viewsPath = possiblePaths.find(p => fs.existsSync(p)) || possiblePaths[0];
+
+  console.log('Resolved viewsPath:', viewsPath);
 
   app.setBaseViewsDir(viewsPath);
   app.setViewEngine('ejs');
