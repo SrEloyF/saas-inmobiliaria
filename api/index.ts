b902926f1473900107f8f53c6bc9f63c3f55f42c
SrEloyF <fabrizioeloys@gmail.com>
2026-03-06 16:26:24 -0500

    vercel settings v11

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,22 @@
-
+require('ejs');
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from '../src/app.module';
 import { join } from 'path';
 
 export default async function handler(req: any, res: any) {
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const viewsPath = join(process.cwd(), 'src/views');
-  console.log('#### ViewsPath:', viewsPath);
+
+  console.log('ViewsPath:', viewsPath);
+
   app.setBaseViewsDir(viewsPath);
   app.setViewEngine('ejs');
 
   await app.init();
 
-  const httpAdapter = app.getHttpAdapter();
-  const expressInstance = httpAdapter.getInstance();
-
-  expressInstance(req, res);
+  const server = app.getHttpAdapter().getInstance();
+  return server(req, res);
 }
