fe7d2df8456138195efc11697bac6c9408fffc41
SrEloyF <fabrizioeloys@gmail.com>
2026-03-06 15:47:55 -0500

    vercel settings v7

--- api/index.ts
+++ api/index.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 export default async function handler(req: any, res: any) {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.setBaseViewsDir(join(process.cwd(), 'views'));
+  app.setBaseViewsDir(join(process.cwd(), 'src', 'views'));
   app.setViewEngine('ejs');
 
   await app.init();
