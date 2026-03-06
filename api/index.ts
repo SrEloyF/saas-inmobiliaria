2da085a3896d37ef87d49d7b33684da714f2875d
SrEloyF <fabrizioeloys@gmail.com>
2026-03-06 15:02:04 -0500

    vercel settings v3

---
+++ api/index.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from '../src/app.module';
+import { join } from 'path';
+
+export default async function handler(req: any, res: any) {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setBaseViewsDir(join(process.cwd(), 'views'));
+  app.setViewEngine('ejs');
+
+  await app.init();
+
+  const httpAdapter = app.getHttpAdapter();
+  const expressInstance = httpAdapter.getInstance();
+
+  expressInstance(req, res);
+}
--- src/main.ts
+++ src/main.ts
@@ -12,19 +12,4 @@ async function bootstrap() {
   await app.listen(process.env.PORT || 3000);
 }
 
-if (process.env.VERCEL) {
-  module.exports = async (req: any, res: any) => {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    app.setBaseViewsDir(join(__dirname, 'views'));
-    app.setViewEngine('ejs');
-
-    await app.init();
-
-    const httpAdapter = app.getHttpAdapter();
-    const expressInstance = httpAdapter.getInstance();
-
-    expressInstance(req, res);
-  };
-} else {
-  bootstrap();
-}
+bootstrap();
