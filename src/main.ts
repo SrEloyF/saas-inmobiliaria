3e79a1b23fbeef142aa1c3d2577072a4774108e1
SrEloyF <fabrizioeloys@gmail.com>
2026-03-06 13:40:33 -0500

    config del proyecto para deploy en vercel

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,27 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 
 async function bootstrap() {
-
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
-bootstrap();
+if (process.env.VERCEL) {
+  module.exports = async (req: any, res: any) => {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.setBaseViewsDir(join(__dirname, 'views'));
+    app.setViewEngine('ejs');
+
+    await app.init();
+
+    const httpAdapter = app.getHttpAdapter();
+    const expressInstance = httpAdapter.getInstance();
+
+    expressInstance(req, res);
+  };
+} else {
+  bootstrap();
+}
